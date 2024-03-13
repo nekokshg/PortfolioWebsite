05f3c4edc922ea113232fcaa0be732a34a01d0f8
nicole <nicolekaneshige@gmail.com>
2024-03-13 14:02:24 -0700

    Fixed font import statements

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-/*introduce the installed fonts into the project*/
-import "@fontsource/outfit";
-import "@fontsource/roboto";
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
