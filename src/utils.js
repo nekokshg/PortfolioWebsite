35d800cc6dba005aec71092a87c2ec582b29170f
nicole <nicolekaneshige@gmail.com>
2024-03-13 12:40:19 -0700

    move assets folder test relative path for images

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 export const getImageUrl = (path) => {
     /* function to construct the absolute path of whatever image is there 
     takes a relative path that is passed to it, and then takes base url of our website appends the relative path onto it and returns the new url*/
-    return new URL(`./assets/${path}`, import.meta.url).href;
+    return new URL(`/assets/${path}`, import.meta.url).href;
 };
